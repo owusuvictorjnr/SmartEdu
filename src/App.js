68ba9e0823b3d040309c7d934085aca91612ac36
owusuvictorjnr <owusuvictor80@gmail.com>
2022-08-20 15:56:27 +0100

    back5 as background picture

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home';
+import { Images } from './components/Images';
+
+function App() {
+  return (
+    <div>
+      <Images/>
+      <Home />
+     
+    </div>
+  )
+}
+
+export default App
